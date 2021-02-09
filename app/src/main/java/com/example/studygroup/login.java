e1ef3e55eea15d8221b8d783fddc4b5d725f8911
gkdbs514 <gkdbs514@naver.com>
2021-02-09 20:41:37 +0900

    comm

--- app/src/main/java/com/example/studygroup/login.java
+++ app/src/main/java/com/example/studygroup/login.java
@@ -3,12 +3,32 @@ package com.example.studygroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
+
+    EditText et_Email, et_Password;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.et_Email_id);
+        findViewById(R.id.et_input_id);
+        findViewById(R.id.et_input_password);
+
+    }
+
+    public void clickbnt_login(View view) {
+    }
+
+    public void clickbnt_joinmembership(View view) {
+    }
+
+    public void clickbtn_naverlogin(View view) {
     }
 }
