d1e69ae43a882c5ebe511a00520c40429308bdfb
하윤 <gkdbs514@naver.com>
2021-02-26 16:47:26 +0900

    0226-2

---
+++ app/src/main/java/com/example/studygroup/intro.java
@@ -0,0 +1,22 @@
+package com.example.studygroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+    public void click_intro_login(View view) {
+        startActivity(new Intent(this,login.class));
+        finish();
+    }
+
+}
